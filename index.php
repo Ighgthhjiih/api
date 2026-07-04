6e4a2165c266dd10ba7219df8390cc68dfa1d2ac
isaque <isaquesevero8305@gmail.com>
2026-07-04 20:39:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+header('Content-Type: application/json');
+
 // ==================== CONFIGURAÇÕES ====================
 define('TIMEOUT', 30);
 define('USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
